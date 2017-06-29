6312d6f8d635c8d26f51ebdb9cf5cbb88e8ee8ce
rafiak <rafal.szczepaniak97@gmail.com>
2017-06-29 22:01:07 +0200

    Program rysuj¹cy wykres wskazowy

---
+++ wykresWskazowy (1)/wykresWskazowy/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tensityTB = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.freqTB = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.resistanceTB = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.inductance = New System.Windows.Forms.TextBox()
+        Me.capityTB = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 77)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(207, 39)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Typ obwodu"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(155, 16)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(46, 17)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "RLC"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(85, 16)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "RL"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(18, 16)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(40, 17)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "RC"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(491, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(15, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "U"
+        '
+        'tensityTB
+        '
+        Me.tensityTB.Location = New System.Drawing.Point(513, 27)
+        Me.tensityTB.Name = "tensityTB"
+        Me.tensityTB.Size = New System.Drawing.Size(62, 20)
+        Me.tensityTB.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(376, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(10, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "f"
+        '
+        'freqTB
+        '
+        Me.freqTB.Location = New System.Drawing.Point(393, 25)
+        Me.freqTB.Name = "freqTB"
+        Me.freqTB.Size = New System.Drawing.Size(61, 20)
+        Me.freqTB.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(6, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(16, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "R"
+        '
+        'resistanceTB
+        '
+        Me.resistanceTB.Location = New System.Drawing.Point(28, 24)
+        Me.resistanceTB.Name = "resistanceTB"
+        Me.resistanceTB.Size = New System.Drawing.Size(65, 20)
+        Me.resistanceTB.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Purple
+        Me.Label4.Location = New System.Drawing.Point(108, 26)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(14, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "L"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Green
+        Me.Label5.Location = New System.Drawing.Point(222, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(15, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "C"
+        '
+        'inductance
+        '
+        Me.inductance.Location = New System.Drawing.Point(128, 26)
+        Me.inductance.Name = "inductance"
+        Me.inductance.Size = New System.Drawing.Size(78, 20)
+        Me.inductance.TabIndex = 9
+        '
+        'capityTB
+        '
+        Me.capityTB.Location = New System.Drawing.Point(243, 26)
+        Me.capityTB.Name = "capityTB"
+        Me.capityTB.Size = New System.Drawing.Size(100, 20)
+        Me.capityTB.TabIndex = 10
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(513, 90)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Oblicz"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 61)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(19, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "    "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(228, 77)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(281, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Rozkład napięć na poszczegółnych elementach  obwodu"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(714, 33)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(210, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Rozkaład napięć na rezsytancji i reaktancji"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1105, 606)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.capityTB)
+        Me.Controls.Add(Me.inductance)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.resistanceTB)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.freqTB)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tensityTB)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tensityTB As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents freqTB As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents resistanceTB As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents inductance As TextBox
+    Friend WithEvents capityTB As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ wykresWskazowy (1)/wykresWskazowy/WindowsApp1/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+
+    Dim graf As Graphics
+    Dim freq, R, L, U, cap, XL, XC, Z As Double
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        inductance.Enabled = True
+        capityTB.Enabled = True
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        capityTB.Enabled = False
+        inductance.Enabled = True
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        inductance.Enabled = False
+        capityTB.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        graf = Me.CreateGraphics()
+    End Sub
+
+    Dim I, skala, cosfi, sinfi, deltax, deltay As Double
+    Dim UR, UL, UC As Double
+    Dim p As New Pen(Color.Blue, 2)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        graf = Me.CreateGraphics()
+        Dim x, y, x1, y1, x0, y0, xk, yk As Single
+        Try
+            freq = Double.Parse(freqTB.Text)
+            U = Double.Parse(tensityTB.Text)
+            R = Double.Parse(resistanceTB.Text)
+            If (RadioButton1.Checked) Then
+                L = 0
+            Else
+                L = Double.Parse(inductance.Text)
+            End If
+            If (RadioButton2.Checked) Then
+                cap = 0
+            Else
+                cap = Double.Parse(capityTB.Text)
+            End If
+        Catch
+            MsgBox("Nie podano przynajmniej jedenej z wartości bądż podano warość nieprawidłową np. literę. Pamiętaj, że separatorem jest przecinek.", MsgBoxStyle.OkOnly, "Błąd")
+            Return
+        End Try
+        If (U = 0) Then
+            MsgBox("Napiecie nie może wynosić 0 ", MsgBoxStyle.OkOnly, "Uwaga")
+            Return
+        End If
+
+        graf.Clear(BackColor)
+        XL = 2 * Math.PI * freq * L
+        If (cap = 0) Then
+            XC = 0
+        Else
+            If (freq = 0) Then
+                MsgBox("Wpisana częśtotliwość to 0 a obwód zawiera kondensator, prąd nie popłynie a całe napięcie odłoży się na kondenatorze ", MsgBoxStyle.OkOnly, "Uwaga")
+                R = 0
+                XC = 1
+            Else
+                XC = -1 / (2 * Math.PI * freq * cap)
+            End If
+
+        End If
+        x0 = 250
+        y0 = Me.Height - 100
+        xk = 250
+        yk = 200
+        skala = (y0 - yk) / U
+        Z = Math.Sqrt((R * R) + ((XL + XC) * (XL + XC)))
+        I = U / Z
+        UR = I * R
+        UL = I * XL
+        UC = I * XC
+        p.Color = Color.Blue
+        graf.DrawLine(p, x0, y0, xk, yk)
+        cosfi = R / Z
+        sinfi = (XC + XL) / Z
+        Label6.Text = "Cos fi wynosi " & Math.Round(cosfi, 2) & " a sin fi " & Math.Round(sinfi, 2) & ", XL wynosi " & Math.Round(XL, 2) & ", XC wynosi " & Math.Round(XC, 2)
+        deltay = cosfi * UR * skala
+        deltax = sinfi * UR * skala
+        x = x0 + deltax
+        y = y0 - deltay
+        p.Color = Color.Red
+        graf.DrawLine(p, x0, y0, x, y)
+        deltay = sinfi * UC * skala
+        deltax = cosfi * UC * skala
+        x1 = x - deltax
+        y1 = y - deltay
+        p.Color = Color.Green
+        graf.DrawLine(p, x, y, x1, y1)
+        deltay = sinfi * UL * skala
+        deltax = cosfi * UL * skala
+        x1 = x - deltax
+        y1 = y - deltay
+        p.Color = Color.Purple
+        graf.DrawLine(p, x, y, x1, y1)
+
+        x0 = 750
+        y0 = Me.Height - 100
+        xk = 750
+        yk = 100
+        skala = (y0 - yk) / U
+        p.Color = Color.Blue
+        graf.DrawLine(p, x0, y0, xk, yk)
+        cosfi = R / Z
+        sinfi = (XC + XL) / Z
+        Label6.Text = "Cos fi wynosi " & Math.Round(cosfi, 2) & " a sin fi " & Math.Round(sinfi, 2) & ", XL wynosi " & Math.Round(XL, 2) & ", XC wynosi " & Math.Round(XC, 2)
+        deltay = cosfi * UR * skala
+        deltax = sinfi * UR * skala
+        x = x0 + deltax
+        y = y0 - deltay
+        p.Color = Color.Red
+        graf.DrawLine(p, x0, y0, x, y)
+        p.Color = Color.Brown
+
+        graf.DrawLine(p, x, y, xk, yk)
+
+    End Sub
+
+End Class
